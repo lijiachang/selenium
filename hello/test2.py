b38fa603f442df51d47c2ec7603eb3e7d204bf01
lijiachang <944581577@qq.com>
2021-03-02 22:32:24 +0800

    2021.02 未测试 第一种情况， 所有的包含敏感词都放在一个特定的html里，搜索引擎和历史记录，只要包含，都扔进去，还有最后的历史记录标题空白的或者是空格的 第二种情况，一个历史记录标题的，或者历史记录标题时间间隔不够day天数的，放在一个html里 最后一种情况，完全没有敏感词，并且时间符合day的时间间隔，放在一个html里

--- hello/test2.py
+++ hello/test2.py
@@ -1,14 +1,32 @@
 # coding:utf-8
 
-def select_more(self, sql, *args):
-    try:
-        if args:
-            self.__dict_cursor.execute(sql, args)
-        else:
-            self.__dict_cursor.execute(sql)
-        res = self.__dict_cursor.fetchall()
-        logger.info('query data success：{}'.format(sql))
-    except Exception as e:
-        logger.error('query data failure：{}'.format(e))
-        raise
-    return res
+import requests
+
+headers_str = """Accept: application/json, text/javascript, */*; q=0.01
+Accept-Encoding: gzip, deflate, br
+Accept-Language: zh-CN,zh;q=0.9
+Connection: keep-alive
+Content-Type: application/x-www-form-urlencoded
+Cookie: bid=-6gENow_E0c; __utmz=30149280.1614418005.3.3.utmcsr=baidu|utmccn=(organic)|utmcmd=organic|utmctr=%23%E5%90%B4%E5%AD%9F%E8%BE%BE%E5%8E%BB%E4%B8%96%23; ap_v=0,6.0; __utma=30149280.2026197266.1611584998.1614418005.1614690806.4; __utmc=30149280; __utmt=1; __utmb=30149280.1.10.1614690806
+Host: m.douban.com
+Origin: https://www.douban.com
+Referer: https://www.douban.com/gallery/topic/27196/?qq-pf-to=pcqq.c2c
+Sec-Fetch-Dest: empty
+Sec-Fetch-Mode: cors
+Sec-Fetch-Site: same-site
+User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36"""
+
+
+def get_headers(header_raw):
+    """
+    通过原生请求头获取请求头字典
+    :param header_raw: {str} 浏览器请求头
+    :return: {dict} headers
+    """
+    return dict(line.strip().split(": ", 1) for line in header_raw.split("\n"))
+
+
+url = 'https://m.douban.com/rexxar/api/v2/gallery/topic/27196/items?from_web=1&sort=hot&start=0&count=20&status_full_text=1&guest_only=0&ck=null'
+
+res = requests.get(url, headers=get_headers(headers_str))
+print res.content.decode('unicode-escape')
